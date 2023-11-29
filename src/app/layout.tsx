418e712cb6b615653a50f0fe1730494ae0d7681b
M-Husein <m.husein27@gmail.com>
2023-11-29 21:33:09 +0700

    feat: update skeleton component, etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 
-import './globals.css'
+import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -3,13 +3,13 @@ export const SkeletonCard = () => {
     <div className="border border-blue-300 shadow rounded-md w-full animate-pulse">
       <div className="flex space-x-4">
         <div className="flex-1 space-y-6 p-4">
-          <div className="h-2 bg-slate-200 rounded"></div>
+          <div className="h-4 bg-slate-200 rounded"></div>
           <div className="space-y-3">
             <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-200 rounded col-span-2"></div>
-              <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+              <div className="h-4 bg-slate-200 rounded col-span-2"></div>
+              <div className="h-4 bg-slate-200 rounded col-span-1"></div>
             </div>
-            <div className="h-2 bg-slate-200 rounded"></div>
+            <div className="h-4 bg-slate-200 rounded"></div>
           </div>
         </div>
       </div>
